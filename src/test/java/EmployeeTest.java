cb0706ab16fb1934342c5131c794ef27fc561f40
lyban <liiabd0711@hotmail.com>
2022-02-19 14:34:03 +0100

    (Red) employee class does not exit

---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+
+
+
+    @BeforeAll
+    static void beforeAll() {
+        person = new employee();
+    }
+}
