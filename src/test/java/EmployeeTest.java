14bdecc9b4683d596d71cf3825346c2eb782d7ba
lyban <liiabd0711@hotmail.com>
2022-02-19 16:04:47 +0100

    (Red) employee class does not exist

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.Test;
 public class EmployeeTest {
 
     @Test
+    public void checkIfEmpolyeeSystemContainsStaff(){
+        empolyee = new EmployeeSystem();
+    }
 
 }
 
