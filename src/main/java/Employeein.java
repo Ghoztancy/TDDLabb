205df3bcf0e658e15ba5060f642d05583373bf04
lyban <liiabd0711@hotmail.com>
2022-02-19 16:31:30 +0100

    (Green) Arraylist created and employee list added to make the test go green-.

---
+++ src/main/java/Employeein.java
@@ -0,0 +1,17 @@
+public class Employeein {
+     String Fname;
+     String Ename;
+     int Age;
+     int EmployeeId;
+     float Salary;
+
+     public Employeein(String Fname, String Ename, int Age, float Salary){
+         this.Fname = Fname;
+         this.Ename = Ename;
+         this.Age = Age;
+         this.Salary = Salary;
+     }
+
+
+
+}
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
 
 public class EmployeeTest {
 
+    private ArrayList<Employeein> Staff = new ArrayList<>();
+
     @Test
     public void checkIfEmpolyeeSystemContainsStaff(){
-        EmpolyeeSystem staff = new EmployeeSystem();
+        Staff.add(new Employeein("lyban", "Abdul", 23, 2300.0f));
+
     }
 
 }
